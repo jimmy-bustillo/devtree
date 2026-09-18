729056727f89a009efd3a7e21840b75115fd0510
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2026-09-18 15:10:48 -0600

    Añade express y routing

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("");
+const express = require("express")
+
+const app = express()
+
+// Routing
+app.get("/", (req, res) => {
+  res.send("Hello World")
+})
+
+app.get("/blog", (req, res) => {
+  res.send("Welcome to the blog")
+})
+
+app.listen(4000, () => {
+  console.log("Server is running on port 4000")
+})
